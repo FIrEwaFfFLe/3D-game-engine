7c0c0f9068b6993031942b2c8e790c2c68663b23
FireWaffle <dimakuznetsov1926@gmail.com>
2023-07-02 18:46:03 +0300

    README.md file

---
+++ README.md
@@ -0,0 +1,21 @@
+#3D game engine
+
+##Start main.py and enjoy! 
+
+Wasd+space+shift - position controls (ctrl to move twice as fast),
+arrow keys - camera controls,
+"g" - print current position and camera facing vector to terminal, 
+"y" - display sed parameters on screen.
+
+##Make your own points and connections
+In main.py you can set your own points in the list "points" using the class
+Point(x, y, z).
+
+In main.py you can set the connections between vertecies
+using a tuple of size two with indexes of first and second points in
+the list "points".
+
+If the game is rendering slowly, you can calibrate the FPS in constants.py
+to some lower value.
+
+##Updates coming soon!
--- main.py
+++ main.py
@@ -10,17 +10,17 @@ def rc():
     return randint(0, 200), randint(0, 200), randint(0, 200)
 
 
-points = [Point(6, i, j) for i in range(-10, 11) for j in range(-10, 11)]
+# points = [Point(6, i, j) for i in range(-10, 11) for j in range(-10, 11)]
 points = [Point(2, 2, 1), Point(2, -2, 1), Point(2, -2, -1), Point(2, 2, -1),
           Point(5, 2, 1), Point(5, -2, 1), Point(5, -2, -1), Point(5, 2, -1)]
-po_cols = [rc() for i in range(100)]
+po_cols = [rc() for i in range(1000)]
 connections = [(0, 1), (1, 2), (2, 3), (0, 3),
                (4, 5), (5, 6), (6, 7), (4, 7),
                (0, 4), (1, 5), (2, 6), (3, 7),
                (0, 5), (1, 4), (0, 7), (3, 4),
                (5, 7), (4, 6), (1, 6), (2, 5),
                (3, 6), (2, 7)]
-connect_cols = [rc() for i in range(100)]
+connect_cols = [rc() for i in range(1000)]
 direct = Direct(Point(0, 0, 0),
                 Vector(1, 0, 0),
                 Vector(0, 1, 0),
